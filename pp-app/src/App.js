3abd8e00f98a7bae8a13b8c7144aa51d70d34777
rashadma <rashadkhm@code.edu.az>
2023-10-26 14:31:31 +0300

    added components and styled components

--- pp-app/src/App.js
+++ pp-app/src/App.js
@@ -1,7 +1,44 @@
 import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Theme.js";
+import Navbar from "./components/navbar";
+import Hero from "./components/heroSection";
+import Skills from "./components/skills";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
 
 function App() {
-  return <div className="App">yarra</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Navbar />
+      <Body>
+        <Hero />
+        <Wrapper>
+          <Skills />
+        </Wrapper>
+      </Body>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ pp-app/src/components/skills/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Skills
